9ca2e5a15818669f9a45d00022f29ab4ec7e32fc
Ruslan.Albu <ruslan.albu@paymentsense.com>
2017-12-14 13:50:31 +0000

    doing an http call

--- FunBrain/src/app/app.module.ts
+++ FunBrain/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule} from "@angular/http";
 
 import { AppComponent } from './app.component';
 import {UserComponent} from './user/user.component';
@@ -10,7 +11,8 @@ import {UserComponent} from './user/user.component';
     UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FunBrain/src/app/user/user.component.ts
+++ FunBrain/src/app/user/user.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response} from '@angular/http';
 
 @Component({
     selector: 'app-user',
     templateUrl: 'user.component.html'
 })
 export class UserComponent implements OnInit {
-    constructor() { }
+  loading: boolean;
+  users: any;
+    constructor(public http: Http) { }
 
     ngOnInit() { }
+
+    private loadData(): boolean {
+      console.log('Load data');
+
+      this.loading = true;
+
+      this.http.request('http://localhost:64884/api/users')
+        .subscribe((res: Response) => {
+        this.users = res.json();
+        this.loading = false;
+        console.log('Users', this.users);
+      });
+
+      return false;
+    }
 
 }
