120ac732717e07724d2e6093cee01623067452c7
Ruslan <ruslan.albu@hotmail.com>
2018-01-03 13:53:47 +0000

    user service spec

---
+++ FunBrain/src/app/user/user-search.service.spec.ts
@@ -0,0 +1,25 @@
+import {UserSearchService} from "./user-search.service";
+import {Observable} from "rxjs";
+import {UserSearchResult} from "./user-search-result";
+describe('User Service', () => {
+  it('get', () => {
+    let mockHttp = jasmine.createSpyObj('mockHttp', ['get']);
+
+    let expectedUsers = [{id: 1}];
+    mockHttp.get.and.returnValue(Observable.of(false));
+
+    let userService = new UserSearchService(mockHttp, 'abc');
+  });
+
+  it('should call http.get with the correct URL', () => {
+
+    const expectedUrl = 'api/users';
+    let mockHttp = jasmine.createSpyObj('mockHttp', ['get']);
+    let sut = new UserSearchService(mockHttp, expectedUrl);
+    sut.getUsers();
+
+    expect(mockHttp.get).toHaveBeenCalledWith(expectedUrl);
+
+    });
+
+});
