44475c9f7c06460a71be48060e2b72d900d29f87
Ruslan <ruslan.albu@hotmail.com>
2018-01-19 13:27:20 +0000

    user main page

--- FunBrain/routes.ts
+++ FunBrain/routes.ts
@@ -2,11 +2,13 @@ import {UserComponent} from "./src/app/user/user.component";
 import {UserDetailsComponent} from "./src/app/users/user-details/user-details.component";
 import {Routes} from "@angular/router";
 import {UserEditComponent} from "./src/app/users/user-edit.component/user-edit.component";
+import {UserListComponent} from "./src/app/users/user-list/user-list.component";
 
 export const appRoutes: Routes = [
-  {path: '', component: UserComponent, pathMatch: 'full'},
-  {path: 'users', component: UserComponent, pathMatch: 'full'},
-  {path: 'users/:id', component: UserDetailsComponent, pathMatch: 'full'},
+  {path: '',  redirectTo: '/users', pathMatch: 'full'},
+  {path: 'users', component:UserListComponent, pathMatch: 'full'},
+  {path: 'usersold', component: UserComponent, pathMatch: 'full'},
+  // {path: 'users/:id', component: UserDetailsComponent, pathMatch: 'full'},
   {path: 'edit-user', component: UserEditComponent}
   // {path: '', redirectTo: '/users', pathMatch: 'full'}
 ];
--- FunBrain/src/app/users/user-list/user-list.component.ts
+++ FunBrain/src/app/users/user-list/user-list.component.ts
@@ -1,11 +1,21 @@
-
 import { Component, OnInit } from '@angular/core';
-import {Http, Response} from '@angular/http';
+import {UserResultModel} from "../user-result.model";
+import {UserService} from "../user.service";
 
 @Component({
   selector: 'user-list',
   templateUrl: 'user-list.component.html'
 })
 export class UserListComponent{
+  private users: UserResultModel[];
+
+  constructor(private userService: UserService){
+  }
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    })
+  }
 
 }
